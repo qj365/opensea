eb67be3707de82a773f2e43c56558b94aa5b54b3
qj365 <quangjunior365@gmail.com>
2023-05-03 18:35:58 +0700

    change env

--- graphql/apollo-client.js
+++ graphql/apollo-client.js
@@ -1,7 +1,10 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-    uri: process.env.NEXT_PUBLIC_GRAPHQL_URI,
+    uri:
+        process.env.NEXT_PUBLIC_ENV === 'production'
+            ? process.env.NEXT_PUBLIC_GRAPHQL_URI
+            : process.env.NEXT_PUBLIC_GRAPHQL_URI_DEV,
     cache: new InMemoryCache(),
 });
 
